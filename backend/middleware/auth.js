f0bbcb8bb8f205d5cc37f7fd003e2bfa36c8c070
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-19 12:54:43 +0530

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const auth = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) return res.status(401).json({ msg: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
