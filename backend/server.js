1c64a59709ca7e1a727a3bf6fdf2ac49cfd42e27
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-19 12:57:00 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+import goalRoutes from "./routes/goals.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/goals", goalRoutes);
+
+app.get("/", (req, res) => res.send("API Running"));
+
+app.listen(5000, () => console.log("Server running"));
